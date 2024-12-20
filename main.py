b10eb3e3a11814d6b299c69b117426b8d65e4bb8
Jyotibansl <117987514+Jyotibansl@users.noreply.github.com>
2024-12-20 22:32:59 +0200

    Updated main.py
    Added matplotlib and plt.show() to call once.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This entrypoint file to be used in development. Start by reading README.md
+import matplotlib.pyplot as plt
 import time_series_visualizer
 from unittest import main
 
@@ -6,6 +7,6 @@ from unittest import main
 time_series_visualizer.draw_line_plot()
 time_series_visualizer.draw_bar_plot()
 time_series_visualizer.draw_box_plot()
-
+plt.show()
 # Run unit tests automatically
 main(module='test_module', exit=False)
